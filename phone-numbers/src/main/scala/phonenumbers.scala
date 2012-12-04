d8d40f7f83d0e92f9cae080b2ffc38a4d8ea801c
Arya Irani <arya.irani@gmail.com>
2012-12-04 14:59:28 -0500

    more specific javadoc

--- phone-numbers/src/main/scala/phonenumbers.scala
+++ phone-numbers/src/main/scala/phonenumbers.scala
@@ -9,28 +9,28 @@ object phonenumbers {
 
   trait Lookup {
     /**
-     * Return one Home phone if known, otherwise one Mobile phone if known
+     * Return the first Home phone if known, otherwise the first Mobile phone if known
      * @param db
      * @return
      */
     def onePersonalPhone(db: PhoneDB): Option[Phone]
 
     /**
-     * Return all Home phones followed by all Mobile phones
+     * Return all Home phones in their existing order, followed by all Mobile phones in their existing order
      * @param db
      * @return
      */
     def allPersonalPhones(db: PhoneDB): List[Phone]
 
     /**
-     * Return one Business phone if known, otherwise one Mobile phone if known
+     * Return the first Business phone if known, otherwise the first Mobile phone if known
      * @param db
      * @return
      */
     def oneBusinessPhone(db: PhoneDB): Option[Phone]
 
     /**
-     * Return all Business phones followed by all Mobile phones
+     * Return all Business phones in their existing order, followed by all Mobile phones in their existing order
      * @param db
      * @return
      */
